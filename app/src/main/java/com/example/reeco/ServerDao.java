d9edd8436caff6b247ac7cb74f951b1333b99fe1
osm1892 <osm189214@gmail.com>
2022-05-22 22:22:08 +0900

    insertOrUpdate 메소드 추가

--- app/src/main/java/com/example/reeco/ServerDao.java
+++ app/src/main/java/com/example/reeco/ServerDao.java
@@ -3,6 +3,7 @@ package com.example.reeco;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -15,6 +16,9 @@ public interface ServerDao {
 
     @Insert
     void insert(Server server);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertOrUpdate(Server server);
 
     @Update
     void update(Server server);
